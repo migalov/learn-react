a4b1d4ccb20555ae6f7e738aaeda3d7f5ad7cf47
Michel Muniagiseny <mafri.michel@gmail.com>
2023-12-09 21:15:05 +0300

    LRN-REACT-2 Modified create user form

--- src/components/screens/home/create-user-form/CreateUserForm.jsx
+++ src/components/screens/home/create-user-form/CreateUserForm.jsx
@@ -3,21 +3,55 @@ import styles from "./CreateUserForm.module.scss";
 
 const CreateUserForm = ({ setUsers }) => {
 
-  const [name, setName] = useState("");
-  const [balance, setBalance] = useState("");
-  const [avatar, setAvatar] = useState("");
+  const clearData = {
+    name: "",
+    balance: "",
+    avatar: ""
+  };
+
+  const [data, setData] = useState(clearData);
 
   const createUser = e => {
     e.preventDefault();
-    setUsers(prev => [{id: prev.length + 1, name, balance, avatar}, ...prev]);
-    
+
+    setUsers(prev => [
+      {
+        id: prev.length + 1,
+        ...data
+      },
+      ...prev
+    ]);
+
+    setData(clearData);
+
   }
 
   return (
     <form className={styles.form}>
-      <input type="text" placeholder="Name" onChange={e => setName(e.target.value)} value={name} />
-      <input type="text" placeholder="Balance" onChange={e => setBalance(e.target.value)} value={balance} />
-      <input type="text" placeholder="Avatar" onChange={e => setAvatar(e.target.value)} value={avatar} />
+      <input
+        type="text"
+        placeholder="Name"
+        onChange={e => setData(prev => ({
+          ...prev, name: e.target.value})
+        )}
+        value={data.name}
+      />
+      <input
+        type="text"
+        placeholder="Balance"
+        onChange={e => setData(prev => ({
+          ...prev, balance: e.target.value})
+        )}
+        value={data.balance}
+      />
+      <input
+        type="text"
+        placeholder="Avatar"
+        onChange={e => setData(prev => ({
+          ...prev, avatar: e.target.value})
+        )}
+        value={data.avatar}
+      />
       <button className="btn" onClick={e => createUser(e)}>Create user</button>
     </form>
   )
